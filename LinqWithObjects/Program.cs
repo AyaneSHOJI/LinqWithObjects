98420be75fd0902eecf428eba100e186a85866e6
AyaneSHOJI <shojiayane@gmail.com>
2022-07-19 13:06:43 +0200

    understand deferred execution

--- LinqWithObjects/Program.cs
+++ LinqWithObjects/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+// a string array is a sequence that implements IEnumerable<string>
+string[] names = new string[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed" };
+
+WriteLine("Deferred execution");
+
+// Question: Which names end with an M?
+
+// LINQ extension method
+var query1 = names.Where(name => name.EndsWith("m"));
+// LINQ query comprehension syntax
+var query2 = from name in names where name.EndsWith("m") select name;
+
+string[] result1 = query1.ToArray(); 
+List<string> result2 = query2.ToList(); 
+
+foreach(string name in query1)
+{
+    WriteLine(name); // output Pam
+    names[2] = "Jimmy"; //change Jim to Jimmy
+    // on the second iteration Jimmy does not end with an M
+}
